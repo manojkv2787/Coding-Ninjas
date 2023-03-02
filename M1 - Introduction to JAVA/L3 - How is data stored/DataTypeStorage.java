f96b1a48a9273121e85ce8c8b38eebebad6d3200
Manoj K V <109405845+manojkv2787@users.noreply.github.com>
2023-03-02 13:30:36 +0530

    Add files via upload

---
+++ M1 - Introduction to JAVA/L3 - How is data stored/DataTypeStorage.java
@@ -0,0 +1,13 @@
+package fundamentals;
+import java.util.Scanner;
+public class DataTypeStorage {
+
+	public static void main(String[] args) {
+		char ch='a';
+	    int i='a'+3;
+	    System.out.println(i);
+	    int min=Integer.MIN_VALUE;
+        int max=Integer.MAX_VALUE;
+        System.out.println(min+" "+max);
+	}
+}
