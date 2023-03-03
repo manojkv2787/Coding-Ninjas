3afbb177868fa03ff23b97a2742db34b3766e428
Manoj K V <109405845+manojkv2787@users.noreply.github.com>
2023-03-03 10:45:39 +0530

    Update AddTwoNumbers.java

--- M1 - Introduction to JAVA/L1 - FlowCharts/AddTwoNumbers.java
+++ M1 - Introduction to JAVA/L1 - FlowCharts/AddTwoNumbers.java
@@ -4,8 +4,8 @@ public class AddTwoNumbers {
 
 	public static void main(String[] args) {
 		double a = 6 / 4;
-        int b  = 6 / 4;
-        double c = a + b;
-        System.out.println(c);
+		int b  = 6 / 4;
+		double c = a + b;
+		System.out.println(c);
 	}
 }
