67894f8484d04a4b1e9779278bac9aaa8892e2dc
Manoj K V <109405845+manojkv2787@users.noreply.github.com>
2023-03-06 23:12:11 +0530

    Add files via upload

---
+++ M1 - Introduction to JAVA/L6 - Patterns 2/MirrorImageNumber.java
@@ -0,0 +1,51 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+The dots represent spaces.
+
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines.
+
+Constraints
+0 <= N <= 50
+Sample Input 1:
+3
+Sample Output 1:
+      1 
+    12
+  123
+Sample Input 2:
+4
+Sample Output 2:
+      1 
+    12
+  123
+1234
+*/
+
+package Solutions;
+import java.util.*;
+
+public class MirrorImageNumber {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		for (int i=0;i<N;i++)
+		{
+			int val=1;
+			for (int j=0;j<(N-i-1);j++)
+			{
+				System.out.print(" ");
+			}
+			for (int j=0;j<=i;j++)
+			{
+				System.out.print(val);
+				val++;
+			}
+			System.out.println(" ");
+		}
+		scan.close();
+	}
+}
